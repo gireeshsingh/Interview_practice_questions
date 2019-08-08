ed8dfd705f0acd9830ffbb93ca50e182e9428422
Gireesh <46069550+gireeshsingh@users.noreply.github.com>
2019-08-09 02:45:11 +0530

    ZeroTriplets

---
+++ ZeroTriplets.java
@@ -0,0 +1,40 @@
+import java.util.*; 
+
+class ZeroTriplets
+{ 
+
+	static void findTriplets(int arr[], int n) 
+	{ 
+		boolean found = false; 
+
+		for (int i = 0; i < n - 1; i++) 
+		{ 
+			HashSet<Integer> s = new HashSet<Integer>(); 
+			for (int j = i + 1; j < n; j++) 
+			{ 
+				int x = -(arr[i] + arr[j]); 
+				if (s.contains(x)) 
+				{ 
+					System.out.printf("%d %d %d\n", x, arr[i], arr[j]); 
+					found = true; 
+				} 
+				else
+				{ 
+					s.add(arr[j]); 
+				} 
+			} 
+		} 
+
+		if (found == false) 
+		{ 
+			System.out.printf(" No Triplet Found\n"); 
+		} 
+	} 
+
+	public static void main(String[] args) 
+	{ 
+		int arr[] = {0, -1, 2, -3, 1}; 
+		int n = arr.length; 
+		findTriplets(arr, n); 
+	} 
+} 
