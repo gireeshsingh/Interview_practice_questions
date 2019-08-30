16ce367170bc29ce2f063c88dfc348c3b412987a
Gireesh <46069550+gireeshsingh@users.noreply.github.com>
2019-08-30 01:33:34 -0400

    arrayToBST.java

--- arrayToBST.java
+++ arrayToBST.java
@@ -1,34 +1,44 @@
-
+class TreeNode {
+	int value;
+	TreeNode left;
+	TreeNode right;
+	TreeNode(int x){
+		value=x;
+	}
+}
 
 public class arrayToBST{
 	public static void main(String args[]){
 		int[] arr={-10,-3,0,5,9};
 		
 		if(arr==null || arr.length==0)
-			return null;
-	
-		return makeBST(arr,0,arr.length-1);
+			return;
+		
+		arrayToBST a2bst= new arrayToBST();
+		TreeNode tn=a2bst.makeBST(arr,0,arr.length-1);
+		
+		a2bst.printTree(tn);
+		
 	}
-}
-
-public TreeNode makeBST(int[] arr, int left, int right){
-	if(left>right)
-		return null;
 	
-	int mid=left+(right-left)/2;
-	
-	TreeNode t=new TreeNode(arr[mid]);
-	t.left=makeBST(arr, left, mid-1);
-	t.right=makeBST(arr, mid+1, right);
-	
-	return TreeNode;
-}
-
-	public class TreeNode {
-		int value;
-		TreeNode left;
-		TreeNode right;
-		Treenode(int x){
-			value=x;
+	public void printTree(TreeNode tn){
+		if(tn!=null){
+			System.out.println(tn.value);
+			printTree(tn.left);
+			printTree(tn.right);
 		}
 	}
+	
+	public TreeNode makeBST(int[] arr, int left, int right){
+		if(left>right)
+			return null;
+		
+		int mid=left+(right-left)/2;
+		
+		TreeNode t=new TreeNode(arr[mid]);
+		t.left=makeBST(arr, left, mid-1);
+		t.right=makeBST(arr, mid+1, right);
+		
+		return t;
+	}
+}
