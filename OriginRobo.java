e658db41cfc1a04703f00661c39dbbc6126ecf6d
Gireesh <46069550+gireeshsingh@users.noreply.github.com>
2019-09-11 09:07:28 -0400

    Update OriginRobo.java

--- OriginRobo.java
+++ OriginRobo.java
@@ -9,17 +9,16 @@ public class OriginRobo{
 	
 	public boolean check(String move){
 		int x=0, y=0;
-		int[] movement= new int[8];
-		
+				
 		for(String i: move.split("")){
-			if(i=="U")
+			if(i.equals("U"))
 				y++;
-			else if(i=="D")
+			else if(i.equals("D"))
 				y--;
-			else if(i=="L")
+			else if(i.equals("L"))
 				x--;
-			else if(i=="R")
-				x++;
+			else if(i.equals("R"))
+				x++;   
 		}
 		
 		return (x==0 && y==0);
