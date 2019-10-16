1b3b0f9843300bc3a1d7a38d9474753d239dddc5
Gireesh <46069550+gireeshsingh@users.noreply.github.com>
2019-10-16 01:09:16 -0400

    SumOfRootToBinaryNumbers.java

--- SumOfRootToBinaryNumbers.java
+++ SumOfRootToBinaryNumbers.java
@@ -1,22 +1,36 @@
-Given a binary tree, each node has value 0 or 1.  Each root-to-leaf path represents a binary number starting with the most significant bit.  For example, if the path is 0 -> 1 -> 1 -> 0 -> 1, then this could represent 01101 in binary, which is 13.
-
-For all leaves in the tree, consider the numbers represented by the path from the root to that leaf.
-
-Return the sum of these numbers.
-
- 
-
-Example 1:
-
-
-
-Input: [1,0,1,0,1,0,1]
-Output: 22
-Explanation: (100) + (101) + (110) + (111) = 4 + 5 + 6 + 7 = 22
- 
-
-Note:
-
-The number of nodes in the tree is between 1 and 1000.
-node.val is 0 or 1.
-The answer will not exceed 2^31 - 1.
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class SumOfRootToBinaryNumbers {
+    public int sumRootToLeaf(TreeNode root) {
+        if(root==null) return 0;
+        List<String> list=helper(root,"");
+        int ans=0;
+        for(String str: list){
+            int i=1, temp=0;
+            for(String s: str.split("")){
+                temp+=i*Integer.parseInt(s);
+                i*=2;
+            }
+            ans+=temp;
+        }
+        return ans;
+    }
+    public List<String> helper(TreeNode root, String str){
+        List<String> list=new ArrayList<String>();
+        if(root==null) return list;
+        if(root.left==null && root.right==null){
+            list.add(root.val+str);
+            return list;
+        }
+        list.addAll(helper(root.left,root.val+str));
+        list.addAll(helper(root.right,root.val+str));
+        return list;
+    }
+}
