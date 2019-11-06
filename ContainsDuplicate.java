83dff465045d8b586bbac354bb1cece83846c01d
Gireesh <46069550+gireeshsingh@users.noreply.github.com>
2019-11-05 23:44:20 -0500

    ContainsDuplicate.java

---
+++ ContainsDuplicate.java
@@ -0,0 +1,10 @@
+class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set= new HashSet<Integer>();
+        for(int i: nums){
+            if(set.contains(i)) return true;
+            else set.add(i);
+        }
+        return false;
+    }
+}
