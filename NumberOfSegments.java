7f268cd0856620b23153778d57dff6dba6fd8164
Gireesh <46069550+gireeshsingh@users.noreply.github.com>
2019-12-05 02:19:32 -0500

    NumberOfSegments

---
+++ NumberOfSegments.java
@@ -0,0 +1,7 @@
+class NumberOfSegments {
+    public int countSegments(String s) {
+        int res=0;
+        for(int i=0;i<s.length();i++)if((i==0||s.charAt(i-1)==' ') && s.charAt(i)!=' ')res++;
+        return res;
+    }
+}
