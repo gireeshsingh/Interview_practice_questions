dcec32abe337eca104382d853d8563cf2b9c5c72
Gireesh <46069550+gireeshsingh@users.noreply.github.com>
2019-12-15 19:51:05 -0500

    TransposeMatrix

---
+++ TransposeMatrix.java
@@ -0,0 +1,12 @@
+class TransposeMatrix {
+    public int[][] transpose(int[][] A) {
+        int temp;
+        int[][] B=new int[A[0].length][A.length];
+        for(int i=0;i<B.length;i++)
+            for(int j=0; j<B[0].length; j++){
+                B[i][j]=A[j][i];
+            }
+
+        return B;
+    }
+}
