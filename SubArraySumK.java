a8401619704d9c913f117f375ee27183b5f0b7de
gireeshsingh <gireeshsinghthakurathi@gmail.com>
2020-01-31 20:58:38 -0500

    SubArraySumK

---
+++ SubArraySumK.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int sum=0, ans=0;
+        HashMap<Integer, Integer> map = new HashMap();
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            if(sum==k) ans++;
+            if(map.containsKey(sum-k)) ans+=map.get(sum-k);
+            map.put(sum, map.getOrDefault(sum,0)+1);
+        }
+        return ans;
+    }
+}
